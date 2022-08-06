83fee3be83b3843bbd89a7449fc1c371924ebaae
Anupom11 <anupom.chakrabarty@gmail.com>
2022-08-06 23:59:18 +0530

    added gradient background

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import StartGameScreen from './Screens/StartScreen';
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
       <StartGameScreen/>
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
